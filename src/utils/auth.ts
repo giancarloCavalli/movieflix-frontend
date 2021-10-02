4e31c5995f126d14e3479b6cee39c778cba63cb4
Giancarlo Cavalli <gc.giancarloo@gmail.com>
2021-10-02 10:04:58 -0300

    Navbar listening to AuthContext

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,3 +1,5 @@
+import jwtDecode from "jwt-decode";
+import { Token } from "typescript";
 import { getAuthData } from "./storage";
 
 export type Role = 'ROLE_VISITOR' | 'ROLE_MEMBER';
@@ -14,6 +16,14 @@ export type TokenData = {
 }
 
 export const isAuthenticated = (): boolean => {
-  const authData = getAuthData();
-  return (authData && authData?.expires_in > (Date.now() / 1000)) ? true : false;
+  const tokenData = getTokenData();
+  return (tokenData && tokenData?.exp > (Date.now() / 1000)) ? true : false;
+}
+
+export const getTokenData = (): TokenData | undefined => {
+  try {
+    return jwtDecode(getAuthData().access_token) as TokenData;
+  } catch (error) {
+    return undefined;
+  }
 }
